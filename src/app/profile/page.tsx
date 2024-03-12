21744e60013b9776debb9439d8327ff762a5e813
paras verma <parasverma2531@gmail.com>
2024-03-12 12:40:06 +0530

    Logout functionality in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,42 @@
 "use client";
-import React from "react";
-import { useRouter } from "next/navigation";
+import axios from "axios";
 import Link from "next/link";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successfull");
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
-      <p>Profile Page</p>
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
