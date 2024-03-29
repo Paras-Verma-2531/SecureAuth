3bfec37de547fa1c4cb77dbc80eb6429ca844d89
paras verma <parasverma2531@gmail.com>
2024-03-29 14:57:02 +0530

    VerifyEmail route built

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,35 @@
+// url :: /api/users/verifyemail
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log(token);
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+    if (!user) return NextResponse.json({ error: "Invalid token" });
+    //valid user exits
+    console.log(user);
+    //make the user verfied
+    user.isVerfied = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+    return NextResponse.json({
+      message: "Email verified successfully",
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error.message,
+      status: 500,
+    });
+  }
+}
