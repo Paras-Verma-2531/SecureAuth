db1b508b8df4ad2e3c33e29e9acfb37cf0d80eb5
paras verma <parasverma2531@gmail.com>
2024-03-30 15:39:19 +0530

    User profile route build

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,23 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getTokenData";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/user";
+
+connect();
+export async function GET(request: NextRequest) {
+  try {
+    const userId=getDataFromToken(request);
+    const user=await User.findOne({_id:userId}).select("-password");
+   const response= NextResponse.json({
+    message:"User found",
+    data:user
+   })
+   return response
+
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error.message,
+      status: 500,
+    });
+  }
+}
