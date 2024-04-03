7c197bee19d8bacb996947cf06e137e78ef4ce2f
paras verma <parasverma2531@gmail.com>
2024-04-03 17:54:28 +0530

    Get User Profile button add-on

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,13 +4,20 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import axiosConfig from "@/helpers/axiosHelper";
 
 export default function ProfilePage() {
   const router = useRouter();
-  const [data, setData] = useState("nothing");
+  const [data, setData] = useState("");
+  const getUserDetails=async ()=>
+  {
+     const response=await axiosConfig.get("/me");
+     const userData=response.data.data;
+     setData(userData._id)
+  }
   const logout = async () => {
     try {
-      await axios.get("/api/users/logout");
+      await axiosConfig.get("/logout");
       toast.success("Logout successfull");
       router.push("/login");
     } catch (error: any) {
@@ -31,6 +38,12 @@ export default function ProfilePage() {
         )}
       </h2>
       <hr />
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User Details
+      </button>
       <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
